bbf6c5f17e4715d67af8ac77bb2af0de7e5064cc
littleGiant-28 <vaibhavthakor2@gmail.com>
2022-06-15 22:44:01 +0530

    adding back slpd, td head

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -31,6 +31,44 @@ class ColorHistogramHead(torch.nn.Module):
         
         return hist_probs
 
+class TextureDiscriminationHead(torch.nn.Module):
+    
+    def __init__(self, in_features, config, **kwargs):
+        super(TextureDiscriminationHead, self).__init__()
+        
+        p_dim = config.model.projection_dim
+        self.projection_head = torch.nn.Sequential(
+            torch.nn.Linear(in_features, p_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(p_dim, p_dim),
+            torch.nn.ReLU()
+        )
+        
+    def forward(self, features):
+        x = self.projection_head(features)
+        x = F.normalize(x, p=2, dim=-1)
+        G = gram_matrix(x)
+        
+        return G
+
+class LocalPatchDiscriminatorHead(torch.nn.Module):
+    
+    def __init__(self, in_features, config, **kwargs):
+        super(LocalPatchDiscriminatorHead, self).__init__()
+        
+        p_dim = config.model.projection_dim
+        self.projection_head = torch.nn.Sequential(
+            torch.nn.Linear(in_features, p_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(p_dim, p_dim),
+            torch.nn.ReLU()
+        )
+        
+    def forward(self, features):
+        x = self.projection_head(features)
+        x = F.normalize(x, p=2, dim=-1)
+        return x
+
 class FeatureExtractor(torch.nn.Module):
     supported_backbones = {
         "resnet50": torchvision.models.resnet50
@@ -49,7 +87,9 @@ class FeatureExtractor(torch.nn.Module):
 
         self.backbone, feature_size = self.get_backbone(config, backbone_name)
         self.colorhist_head = ColorHistogramHead(feature_size, config)
-        
+        self.td_head = TextureDiscriminationHead(feature_size, config)
+        self.slpd_head = LocalPatchDiscriminatorHead(feature_size, config)
+
     def get_backbone(self, config, backbone_name):
         """
         Create backbone, remove last fc layer from backbone and return it
@@ -59,11 +99,32 @@ class FeatureExtractor(torch.nn.Module):
         )
         backbone = torch.nn.Sequential(*(list(backbone.children())[:-1]))
         return backbone, self.feature_sizes[config.model.backbone]
-    
-    def forward(self, image):
+
+    def get_slpd_features(self, image_patch):
+        patch_features = self.backbone(image_patch)
+        patch_features = patch_features.reshape(-1, patch_features.shape[1])
+        slpd_features = self.slpd_head(patch_features)
+        
+        return slpd_features
+
+    def get_td_features(self, image):
+        features = self.backbone(image)
+        features = features.reshape(-1, features.shape[1])
+        td_features = self.td_head(features)
+        
+        return td_features
+
+    def forward(self, image, image_patch):
         features = self.backbone(image)
         
         features = features.reshape(-1, features.shape[1])
         global_hist_probs = self.colorhist_head(features)
+        texture_features = self.td_head(features)
         
-        return global_hist_probs
+        patch_features = self.backbone(image_patch)
+        patch_features = patch_features.reshape(-1, patch_features.shape[1])
+        patch_hist_probs = self.colorhist_head(patch_features)
+        slp_features = self.slpd_head(patch_features)
+        
+        return global_hist_probs, texture_features, \
+            slp_features, patch_hist_probs
